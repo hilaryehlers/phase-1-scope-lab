a126fe26864d17943b4bd9a87b1b8218fceb77e8
Hilary Ehlers <hilarydixson@gmail.com>
2024-06-01 11:29:40 -0500

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase();
+console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+console.log(bestCustomer)
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer)
+}
+
+const leastFavoriteCustomer = 'brenda';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'alison';
+    console.log(leastFavoriteCustomer)
+}
